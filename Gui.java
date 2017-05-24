16d4bead3bbddf9dd6ca056275f83ecece684839
Victor <victorzhengvz@gmail.com>
2017-05-24 16:28:06 -0400

    added acition listener to submit button, included rudimentary input check with dynamic JLabel msg

--- Gui.java
+++ Gui.java
@@ -2,17 +2,26 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-
+@SuppressWarnings("serial")
 public class Gui extends JPanel{ 
 	// private JLayeredPane layeredPane;
 	// private JLayeredPane layeredPane2;
   private static final int WIDTH = 175;
-  private static final int HEIGHT_TOP = 225;
+  private static final int HEIGHT_TOP = 215;
   private static final int HEIGHT_BOT = 225;
   private static final Color BG_COLOR = Color.CYAN;
+
+  private String search;
+  private String email;
+  private JLabel inv_search_inp = new JLabel("Enter a search query");
+  private JLabel inv_email_inp = new JLabel("Enter a valid email address");
+  private boolean valid_search = true;
+  private boolean valid_email = true;
   ImageIcon submitButtonIcon = new ImageIcon("imgs/submit.png");
 	private JPanel top;
 	private JPanel bot;
+  private JTextField search_field;
+  private JTextField email_field;
 	public Gui(){
     top = new JPanel(new GridLayout(0,1));
     bot = new JPanel();
@@ -37,22 +46,69 @@ public class Gui extends JPanel{
     top.setBorder(BorderFactory.createTitledBorder("top"));
     top.setBackground(BG_COLOR);
     // Create the label + text fields
-    JTextField search_field = new JTextField(15);
-    JTextField email_field = new JTextField(15); 
+    search_field = new JTextField(15);
+    email_field = new JTextField(15); 
     JButton submit = new JButton("Submit");
     submit.setIcon(resizeIcon(submitButtonIcon, 3));
+    submit.addActionListener(new ActionListener(){
+      public void actionPerformed(ActionEvent e){
+        submitButtonPressed();
+      }
+    });
     // Add items to the top panel
     top.add(new JLabel("Search: "));
     top.add(search_field);
     top.add(new JLabel("E-mail: "));
     top.add(email_field);
     top.add(submit);
+
   }
   private void createBotWindow(){
     bot.setPreferredSize(new Dimension(WIDTH, HEIGHT_BOT));
     bot.setBorder(BorderFactory.createTitledBorder("bot"));
     // bot.setBackground(BG_COLOR);
   }
+
+  public void submitButtonPressed(){
+    // update search and email string, change this to update only when valid 
+    search = search_field.getText();
+    email = email_field.getText();
+
+
+    // rudimentary input check
+    // empty search
+    if (search_field.getText().equals("")){
+      // add label only if it doesnt exist before 
+      if (valid_search){
+        top.add(inv_search_inp); // how to dynamically remove?
+        valid_search = false;
+        top.revalidate();
+        top.repaint();
+      }
+    }else{
+      //VALID INPUT
+      valid_search = true;
+      top.remove(inv_search_inp);
+      top.validate();
+      top.repaint();
+    }
+    // email does not contain @
+    if (!email_field.getText().contains("@")){
+      if (valid_email){
+        top.add(inv_email_inp);
+        valid_email = false;
+        top.revalidate();
+        top.repaint();
+      }
+    }else{
+      // VALID INPUT
+      valid_email = true;
+      top.remove(inv_email_inp);
+      top.validate();
+      top.repaint(); 
+    }
+  }
+
 	private static void createAndShowGUI() {
 
         //Create and set up the window.
