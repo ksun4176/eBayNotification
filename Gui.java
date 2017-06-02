9fb076fd21c1eafa4737837527ae5cd6137755d1
kevin <kevin.sun.1@stonybrook.edu>
2017-06-02 19:38:18 -0400

    update html file depending on search

--- Gui.java
+++ Gui.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
+import java.util.Arrays;
 
 @SuppressWarnings("serial")
 public class Gui extends JPanel{
@@ -24,6 +26,12 @@ public class Gui extends JPanel{
 
   //submit button component of top pane
   ImageIcon submitButtonIcon = new ImageIcon("imgs/submit.png");
+
+  //html file used for bottom pane
+  private String htmlname = "results.html";
+  private BufferedReader br = new BufferedReader(new FileReader("resultshtml.tmp"));
+  private String fileParts[3];
+  Arrays.fill(fileParts,"");
 
 	private JPanel top;
 	private JPanel bot;
@@ -104,30 +112,35 @@ public class Gui extends JPanel{
       top.validate();
       top.repaint(); 
     }
+    
+    updateFile(search);
   }
 
 	private static void createAndShowGUI() {
 
-        //Create and set up the window.
-        JFrame frame = new JFrame("eBay Notification");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //Create and set up the window.
+      JFrame frame = new JFrame("eBay Notification");
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JComponent newContentPane = new Gui();
-        newContentPane.setOpaque(true); //content panes must be opaque
-        frame.setContentPane(newContentPane);
+      JComponent newContentPane = new Gui();
+      newContentPane.setOpaque(true); //content panes must be opaque
+      frame.setContentPane(newContentPane);
 
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
+      //Display the window.
+      frame.pack();
+      frame.setVisible(true);
   }
   public static void main(String[] args) {
-      //Schedule a job for the event-dispatching thread:
-      //creating and showing this application's GUI.
-      javax.swing.SwingUtilities.invokeLater(new Runnable() {
-          public void run() {
-              createAndShowGUI();
-          }
-      });
+
+    fillParts();
+
+    //Schedule a job for the event-dispatching thread:
+    //creating and showing this application's GUI.
+    javax.swing.SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        createAndShowGUI();
+      }
+    });
   }
 
 
@@ -137,6 +150,33 @@ public class Gui extends JPanel{
     Image resizedImage = img.getScaledInstance(img.getWidth(null)/factor, img.getHeight(null)/factor,  java.awt.Image.SCALE_SMOOTH);  
     return new ImageIcon(resizedImage);
   }
+  private static void fillParts(){
+    int parts = 0;
+    String line;
+    while((line = br.readLine()) != null){
+      if(line.contains("@keywords@") == true){
+        fileParts[++parts] = line;
+        parts++;
+      } else{
+        fileParts[parts] += line;
+      }
+    }
+    br.close();
+  }
 
-  private 
+  private static void updateFile(String repl){
+    htmlname = repl.replaceAll("\s","+") + ".html";
+    try{
+      BufferedWriter bw = new BufferedWriter(new FileWriter(htmlname));
+      bw.write(fileParts[0],0,fileParts[0].length());
+      String temp = fileParts[1];
+      temp.replace("@keywords@",repl);
+      bw.write(temp,0,temp.length());
+      bw.write(fileParts[2],0,fileParts[2].length());
+      bw.close();
+    } catch(IOException e){
+      System.out.println("Error in writing to html file");
+      System.exit(0);
+    }
+  }
 }
